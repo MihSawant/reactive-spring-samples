d0ba9377baaf41d38364ec9df1ab608f8ab216c5
Mihir Sawant <mihirssawant06@gmail.com>
2022-11-02 18:45:19 +0530

    Removed unwanted code

--- src/main/java/sawant/mihir/reactivesamples/webclient/Gimme.java
+++
@@ -1,6 +0,0 @@
-package sawant.mihir.reactivesamples.webclient;
-
-public record Gimme(String postLink, String subreddit, String title,
-                    String url, boolean nsfw, boolean spoiler,
-                    String author, int ups, String[] preview) {
-}
